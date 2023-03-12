5202231055aa5d0d8d7e4d4d73bc63545a6c26da
jorgenbuilder <jorgen@buildnode.io>
2023-03-12 11:18:57 -0700

    refactor: remove dependency on Buffer

--- ext/index.ts
+++ ext/index.ts
@@ -1,26 +1,27 @@
 import { Principal } from '@dfinity/principal';
 import { from32bits, to32bits, toHexString } from '../bits';
 
-
-export function encodeTokenIdentifier(
-    canister : string,
-    index    : number,
-) : string {
-    const padding = Buffer.from("\x0Atid");
+export function encodeTokenIdentifier(canister: string, index: number): string {
+    const encoder = new TextEncoder();
+    const padding = encoder.encode('\x0Atid');
     const array = new Uint8Array([
         ...padding,
         ...Principal.fromText(canister).toUint8Array(),
         ...to32bits(index),
     ]);
     return Principal.fromUint8Array(array).toText();
-};
+}
 
-export function decodeTokenIdentifier(
-    tid: string,
-) : { index : number, canister : string } {
+export function decodeTokenIdentifier(tid: string): {
+    index: number;
+    canister: string;
+} {
+    const encoder = new TextEncoder();
+
+    const correctPadding = encoder.encode('\x0Atid');
     const bytes = Principal.fromText(tid).toUint8Array();
     const padding = Array.from(bytes.subarray(0, 4));
-    if (toHexString(padding) !== toHexString(Array.from(Buffer.from("\x0Atid")))) {
+    if (toHexString(padding) !== toHexString(Array.from(correctPadding))) {
         throw new Error(`Invalid token identifier "${tid}"`);
     } else {
         return {
@@ -28,4 +29,4 @@ export function decodeTokenIdentifier(
             canister: Principal.fromUint8Array(bytes.subarray(4, -4)).toText(),
         };
     }
-};
+}
