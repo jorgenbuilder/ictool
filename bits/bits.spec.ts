e08197075eeefa5d8f1904b9c68ab8bcedc3b86f
jorgenbuilder <jorgen@buildnode.io>
2023-06-24 12:12:03 -0700

    add test

--- bits/bits.spec.ts
+++ bits/bits.spec.ts
@@ -31,4 +31,11 @@ describe('array buffer equality', () => {
         const a2 = new Uint8Array([6, 1, 4, 3, 4]).buffer;
         expect(arrayBufferEqual(a1, a2)).toBe(false);
     });
+
+    it('encodes string to bits', () => {
+        const result = fromHexString(
+            '8d669304c2d9cdbeb2702da288cf24a24c2f2abe310c6884af28b42dcbcaaa59'
+        );
+        expect(result).toBeTruthy();
+    });
 });
